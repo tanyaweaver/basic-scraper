2efb1e4fb389f3f429456dccd9ebb184bec4b133
tanyaweaver <tanya83@uw.edu>
2016-09-02 18:51:44 -0700

    done

--- README.md
+++ README.md
@@ -1,1 +1,33 @@
-# basic-scraper
+# Basic-scraper
+## A python script that scrapes restaurant health inspection data from the King County government website.
+### Data is returned in the following format for a single location:
+```
+{'': ['SEATTLE, WA 98117', 'SEATTLE, WA 98117'],
+'Phone': ['(206) 318-1575', '(206) 318-1575'],
+'Latitude': ['47.7019242696', '47.7019242696'],
+'Longitude': ['122.3636914535', '122.3636914535'],
+'Business Name': ['STARBUCKS COFFEE #3346', 'STARBUCKS COFFEE #3346'],
+'Business Category': ['Seating 13-50 - Risk Category II', 'Seating 13-50 - Risk Category II'],
+'Address': ['9999 HOLMAN RD NW', '9999 HOLMAN RD NW']} 
+ {'Total Inspections': 1, 'Average Score': 30.0, 'High Score': 30} 
+ ```
+### To run a test version, using a static html (src/inspection_page.html):
+```
+$python scraper.py test
+```
+### To use an API call:
+```
+$python scraper.py
+
+### The parameters for the search are at the bottom of scraper.py and can be adjusted:
+```
+if __name__ == "__main__":
+    kwargs = {
+        'Inspection_Start': '1/1/2013',
+        'Inspection_End': '1/10/2013',
+        'City': 'Seattle',
+        'Zip_code': '98108'
+    }
+```
+### sources:
+* https://codefellows.github.io/sea-python-401d4/assignments/tutorials/scraper.html
---
+++ src/scraper.py
@@ -0,0 +1,187 @@
+import requests
+import sys
+from bs4 import BeautifulSoup
+import re
+
+DOMAIN_NAME = 'http://info.kingcounty.gov'
+PATH = '/health/ehs/foodsafety/inspections/Results.aspx'
+QUERY_PARAMS = {
+    'Output': 'W',
+    'Business_Name': '',
+    'Business_Address': '',
+    'Longitude': '',
+    'Latitude': '',
+    'City': '',
+    'Zip_Code': '',
+    'Inspection_Type': 'All',
+    'Inspection_Start': '',
+    'Inspection_End': '',
+    'Inspection_Closed_Business': 'A',
+    'Violation_Points': '',
+    'Violation_Red_Points': '',
+    'Violation_Descr': '',
+    'Fuzzy_Search': 'N',
+    'Sort': 'H',
+}
+
+
+def get_inspection_page(**kwargs):
+    """
+    Make a request to the King County server.
+    Fetch a set of search results. Takes query parameters as arguments.
+    Return the bytes content of the response and encoding.
+    Raise an error if there is a problem with the response.
+    """
+    url = DOMAIN_NAME + PATH
+    params = QUERY_PARAMS.copy()
+    for key, val in kwargs.items():
+        if key in QUERY_PARAMS:
+            params[key] = val
+    response = requests.get(url, params=params)
+    response.raise_for_status()
+    return response.content, response.encoding
+
+
+def load_inspection_page(path):
+    """
+    Read a static html (inspection_page.html).
+    Return the bytes content and the encoding.
+    Used for testing.
+    """
+    import io
+    files = io.open(path, 'rb')
+    response = files.read()
+    files.close()
+    encoding = 'utf-8'
+    return response, encoding
+
+
+def parse_source(html, encoding='utf-8'):
+    """
+    Parse the rsponse body using BeautifulSoup.
+    Return the parsed object for further processing.
+    """
+    parsed = BeautifulSoup(html, 'html5lib', from_encoding=encoding)
+    return parsed
+
+
+def extract_data_listings(html):
+    """
+    Takes in the parsed HTML. Return a list of the restaurant listing
+    container nodes.
+    """
+    id_finder = re.compile(r'PR[\d]+~')
+    return html.find_all('div', id=id_finder)
+
+
+def has_two_tds(element):
+    """
+    Takes an element as an argument.
+    Return 'True' if the element is both a <tr> and contains
+    two <td> elements within it, 'False' otherwise.
+    """
+    elem_is_tr = element.name == 'tr'
+    elem_has_td_children = element.find_all('td', recursive=False)
+    has_two_td = len(elem_has_td_children) == 2
+    return elem_is_tr and has_two_td
+
+
+def clean_data(element):
+    """
+    Takes a cell as an argument. Return the tag.string attribute
+    with extraneous characters stripped.
+    """
+    data = element.string
+    try:
+        return data.strip(" \n:-")
+    except AttributeError:
+        return u""
+
+
+def extract_restaurant_metadata(listing):
+    """
+    Take in the listing for a single restaurant.
+    Return a Python dict with metadata:
+    {<label>: <value>}.
+    """
+    listing_dict = {}
+    metadata_rows = listing.find('tbody').find_all(
+        has_two_tds, recursive=False
+    )
+    for row in metadata_rows:
+        for td in row.find_all('td', recursive=False):
+            key_row, val_row = row.find_all('td', recursive=False)
+            label_present = clean_data(key_row)
+            if label_present:
+                label_dict = label_present
+            else:
+                label_dict = ''
+            listing_dict.setdefault(label_dict, []).append(clean_data(val_row))
+    return listing_dict
+
+
+def is_inspection_row(element):
+    """A filter function. Take in an HTML element.
+    Return 'True' if the element matches the criteria:
+    1 - is a <tr> element,
+    2 - contains 4 <td> elements,
+    3 - word 'inspection' is in the text of it's first <td>,
+    4 - word 'inspection' is not the 1st word in that <td>.
+    Return 'False' otherwise.
+    """
+    is_tr = element.name == 'tr'
+    if not is_tr:
+        return False
+    td_children = element.find_all('td', recursive=False)
+    has_four_td = len(td_children) == 4
+    first_td_text = clean_data(td_children[0]).lower()
+    contains_word = 'inspection' in first_td_text
+    does_not_start = not first_td_text.startswith('inspection')
+    return is_tr and has_four_td and contains_word and does_not_start
+
+
+def extract_score_data(element):
+    """
+    Take a listing as an argument. Use a filter to find inspection rows.
+    Return a dict containing the average score, high score,
+    total inspection values.
+    """
+    inspection_rows = element.find_all(is_inspection_row)
+    samples = len(inspection_rows)
+    total = high_score = average = 0
+    for row in inspection_rows:
+        strval = clean_data(row.find_all('td')[2])
+        try:
+            intval = int(strval)
+        except (ValueError, TypeError):
+            samples -= 1
+        else:
+            total += intval
+            high_score = intval if intval > high_score else high_score
+    if samples:
+        average = total/float(samples)
+    data = {
+        u'Average Score': average,
+        u'High Score': high_score,
+        u'Total Inspections': samples
+    }
+    return data
+
+
+if __name__ == "__main__":
+    kwargs = {
+        'Inspection_Start': '1/1/2013',
+        'Inspection_End': '1/10/2013',
+        'City': 'Seattle',
+        'Zip_code': '98108'
+    }
+if len(sys.argv) > 1 and sys.argv[1] == 'test':
+    html, encoding = load_inspection_page('src/inspection_page.html')
+else:
+    html, encoding = get_inspection_page(**kwargs)
+doc = parse_source(html, encoding)
+listings = extract_data_listings(doc)
+for listing in listings:
+    metadata = extract_restaurant_metadata(listing)
+    score_data = extract_score_data(listing)
+    print(metadata, '\n', score_data, '\n')
